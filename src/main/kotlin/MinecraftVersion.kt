599b0ae2a86df05d8e410f076ee844481ffda831
Light_Quanta <zch22248@foxmail.com>
2024-01-19 23:17:57 +0800

    添加Minecraft版本信息读取

---
+++ src/main/kotlin/MinecraftVersion.kt
@@ -0,0 +1,83 @@
+package tech.lq0.modSyncNext
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import java.io.File
+
+const val baseDir = ".minecraft/versions"
+
+data class MinecraftVersionInfo(
+    val name: String,
+    val version: String,
+    val modLoader: ModLoaderInfo,
+)
+
+data class ModLoaderInfo(
+    val type: ModLoaderType,
+    val version: String,
+)
+
+enum class ModLoaderType {
+    Forge,
+    Fabric,
+    Vanilla,
+}
+
+fun getVersionInfo(name: String): MinecraftVersionInfo? {
+    if (!File("$baseDir/$name/$name.json").exists()) return null
+    val versionInfo = Json.parseToJsonElement(File("$baseDir/$name/$name.json").readText())
+    val args = versionInfo.jsonObject["arguments"]?.jsonObject?.get("game")?.jsonArray ?: return null
+    val strArgs = args.map { it.toString() }
+
+    // Forge
+    if (strArgs.any { "--fml.forgeVersion" in it }) {
+        val forgeVersion = strArgs[strArgs.indexOf("\"--fml.forgeVersion\"") + 1].trim('"')
+        val minecraftVersion = strArgs[strArgs.indexOf("\"--fml.mcVersion\"") + 1].trim('"')
+        return MinecraftVersionInfo(
+            name,
+            minecraftVersion,
+            ModLoaderInfo(
+                ModLoaderType.Forge,
+                forgeVersion
+            )
+        )
+    }
+
+    val mcVersion = (versionInfo.jsonObject["gameVersion"]
+        ?: versionInfo.jsonObject["clientVersion"])?.jsonPrimitive?.content ?: return null
+
+    // Fabric
+    val libraries = versionInfo.jsonObject["libraries"]?.jsonArray
+    if (libraries?.any { "net.fabricmc:fabric-loader:" in it.jsonObject["name"].toString() } == true) {
+        val fabricVersion = libraries.first { "net.fabricmc:fabric-loader:" in it.jsonObject["name"].toString() }
+            .jsonObject["name"]
+            .toString()
+            .trim('\"')
+            .takeLastWhile { it != ':' }
+        return MinecraftVersionInfo(
+            name,
+            mcVersion,
+            ModLoaderInfo(
+                ModLoaderType.Fabric,
+                fabricVersion
+            )
+        )
+    }
+    return MinecraftVersionInfo(
+        name,
+        mcVersion,
+        ModLoaderInfo(
+            ModLoaderType.Vanilla,
+            mcVersion
+        )
+    )
+}
+
+fun getVersionList(): List<MinecraftVersionInfo> {
+    val versions = File(baseDir).listFiles()
+        ?.filter { it.isDirectory }
+        ?.map { it.name }
+    return versions?.mapNotNull(::getVersionInfo) ?: listOf()
+}
